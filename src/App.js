379769eb87a99796c0847cf3acfd5dfa70932a3f
lnthang <lnthangit2407@gmail.com>
2022-08-07 19:56:37 +0700

    update ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
+import { useEffect, useState } from "react";
 import ExpenseItem from "./components/ExpenseItem";
-import { item } from "./mockData/mockItem";
+import CreateNewItem from "./components/NewItem/CreateNewItem";
+import { items } from "./mockData/mockItem";
 
 function App() {
+  const [itemData, setItemData] = useState(items);
+
+  useEffect(() => {
+    if (items.length) {
+      setItemData(items);
+    }
+  }, []);
+
+  const handleAddItem = (item) => {
+    // const size =  Object.keys(itemData).length;
+    // setItemData([...itemData, item]);
+
+    // cách 2
+    setItemData((prevState) => {
+      return [item, ...prevState];
+    });
+  };
+
+  const handleFilter = (filterValue) => {
+    if (filterValue) {
+      const newList = items.filter((item) => item.status === filterValue);
+      setItemData(newList);
+    } else {
+      setItemData([...itemData]);
+    }
+  };
+
   return (
     <>
+      <CreateNewItem handleAddItem={handleAddItem} itemLength={items.length} />
       {/* có 2 cách để thêm 1 components */}
       {/* cách 1 */}
-      <ExpenseItem item={item} />
+      <ExpenseItem items={itemData} handleFilterStatus={handleFilter} />
       {/* cách 2 */}
       {/* <ExpenseItem></ExpenseItem> */}
     </>
